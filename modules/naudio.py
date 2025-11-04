006c5a22082c2eced55955ddc70b55e4dd358c88
Nan0b1 <pikajoss2009@gmail.com>
2025-11-04 21:33:16 +0100

    code cleaned

--- modules/naudio.py
+++ modules/naudio.py
@@ -2,9 +2,7 @@ import os
 from soundfile import read, write
 
 
-def convert_to_wav(dirpath, name, extension):
-    #if not os.path.exists(dirpath):
-    #    os.makedirs(dirpath)
+def convert_to_wav(dirpath, name, extension) -> str:
     if extension == ".ogg":
         data, samplerate = read(dirpath + name)
         write(os.path.join(dirpath + name + ".wav"), data, samplerate)
--- modules/nrvc.py
+++ modules/nrvc.py
@@ -5,7 +5,6 @@ class NRVC():
     def __init__(self):
         self.rvc = RVCInference(device="cuda:0")
         self.rvc.load_model("models/villager/villager.pth", index_path="models/villager/added_IVF81_Flat_nprobe_1_v2.index")
-        #self.rvc.set_params(index_rate="0.5", filter_radius=0.2, f0method="harvest", resample_sr=48000, rms_mix_rate=0.2, protect=0.1, f0up_key=0)
         self.rvc.set_params(filter_radius=0.2, f0method="harvest", resample_sr=8000, rms_mix_rate=0.2, protect=0.2, f0up_key=0)
 
     def infer_file(self, dirpath, name, extension, outname):

f009e6e099d555ec081b1b60e772932271d2931e
Nan0b1 <pikajoss2009@gmail.com>
2025-11-05 15:19:52 +0100

    naudio reworked + documentation on this one

--- modules/naudio.py
+++ modules/naudio.py
@@ -1,9 +1,24 @@
-import os
 from soundfile import read, write
 
 
-def convert_to_wav(dirpath, name, extension) -> str:
-    if extension == ".ogg":
-        data, samplerate = read(dirpath + name)
-        write(os.path.join(dirpath + name + ".wav"), data, samplerate)
-    return dirpath + name + ".wav"
+def convert_to_wav(dirpath:str, name:str, extension:str, outextension:str, outdirpath:str="", outname:str="") -> str:
+    """
+    :param dirpath: The directory path of the input audio file
+    :param name: The name of the input audio file without the extension
+    :param extension: The extension of the input audio file with a dot
+    :param outdirpath: The directory path of the output audio file
+    :param outname: The name of the output audio file without the extension
+    :param outextension: The extension of the output audio file with a dot
+    :return: The path of the output audio file
+    """
+
+    if outname=="":
+        outname=name
+    if outdirpath=="":
+        outdirpath=dirpath
+
+    if extension != outextension:
+        data, samplerate = read(f"{dirpath}\\{name}{extension}")
+        write(f"{outdirpath}\\{outname}{outextension}", data, samplerate)
+
+    return f"{outdirpath}\\{outname}{outextension}"
